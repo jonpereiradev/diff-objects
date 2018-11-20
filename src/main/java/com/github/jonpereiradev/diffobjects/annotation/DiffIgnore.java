326d66f9ba25942a1dbf0419d4ad5384281eec20
Jonathan Pereira <jonpereiradev@gmail.com>
2018-11-20 14:53:10 -0200

    Diff ignore annotation to ignore methods when DiffMappings on class.

---
+++ src/main/java/com/github/jonpereiradev/diffobjects/annotation/DiffIgnore.java
@@ -0,0 +1,17 @@
+package com.github.jonpereiradev.diffobjects.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to ignore a mapping when using {@link DiffMappings} on a class to map all fields.
+ *
+ * @author Jonathan Pereira
+ * @since 1.1
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface DiffIgnore {
+}
